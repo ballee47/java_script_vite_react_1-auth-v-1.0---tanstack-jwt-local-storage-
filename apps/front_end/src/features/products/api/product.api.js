89b5f92522ff9081d8ad988cab1faf7067e6a546
ballee47@gmail.com <ballee47@gmail.com>
2026-04-30 18:33:23 -0500

    1.3

--- apps/front_end/src/features/products/api/product.api.js
+++
@@ -1,9 +0,0 @@
-import { httpClient } from "@/infra/http/httpClient";
-export const createProduct = async (data) => {
-    const response = await httpClient.post("/products/", data);
-    return response.data;
-};
-export const getProducts = async () => {
-    const response = await httpClient.get("/products/");
-    return response.data;
-};
--- apps/front_end/src/features/products/services/productService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const API = axios.create({
-    baseURL: "http://localhost:8000/api",
-});
-export const getProducts = async () => {
-    const res = await API.get("/products");
-    return res.data;
-};
