3e7d5e5ecccfea4f4b65d940557be424363bef5a
ballee47@gmail.com <ballee47@gmail.com>
2026-07-13 16:50:07 -0500

    storage 1.3 (constants)

---
+++ apps/front_end/src/infra/storage/constants/index.ts
@@ -0,0 +1,6 @@
+export * from "./StorageDefaults";
+export * from "./errors/StorageErrors";
+export * from "./StorageKeys";
+export * from "./limits/StorageLimits";
+export * from "./StorageNamespaces";
+export * from "./StorageVersions";
